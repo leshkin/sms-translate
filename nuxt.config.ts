03b4a2fef8cd7a5ba5eea7c3ed52dd04f05196ae
leshkin <alaposhin@gmail.com>
2022-11-10 15:21:38 +0300

    fix: link paths

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,10 +10,10 @@ export default defineNuxtConfig({
       title: 'Georgian SMS Translation',
       description: 'Translation of Georgian SMS written with transliteration',
       link: [
-        {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png'},
-        {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png'},
-        {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png'},
-        {rel: 'manifest', href: '/site.webmanifest'}
+        {rel: 'apple-touch-icon', sizes: '180x180', href: '/sms-translate/apple-touch-icon.png'},
+        {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/sms-translate/favicon-32x32.png'},
+        {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/sms-translate/favicon-16x16.png'},
+        {rel: 'manifest', href: '/sms-translate/site.webmanifest'}
       ]
     },
   },
