c6a13baa096dd971b7e68dd059f8c8f089620fea
leshkin <alaposhin@gmail.com>
2022-11-10 16:47:14 +0300

    refactor: Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,23 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
+const path = (process.env.NODE_ENV === 'development') ? '/' : '/sms-translate/'
+
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
   app: {
-    baseURL: process.env.NODE_ENV === 'development' ? '/' : '/sms-translate/',
+    baseURL: path,
     head: {
       charset: 'utf-16',
       viewport: 'width=device-width, initial-scale=1.0', 
       title: 'Georgian SMS Translation',
       description: 'Translation of Georgian SMS written with transliteration',
       link: [
-        {rel: 'apple-touch-icon', sizes: '180x180', href: '/sms-translate/apple-touch-icon.png'},
-        {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/sms-translate/favicon-32x32.png'},
-        {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/sms-translate/favicon-16x16.png'},
-        {rel: 'manifest', href: '/sms-translate/site.webmanifest'}
+        {rel: 'apple-touch-icon', sizes: '180x180', href: path + 'apple-touch-icon.png'},
+        {rel: 'icon', type: 'image/png', sizes: '32x32', href: path + 'favicon-32x32.png'},
+        {rel: 'icon', type: 'image/png', sizes: '16x16', href: path + 'favicon-16x16.png'},
+        {rel: 'manifest', href: path + 'site.webmanifest'}
       ]
     },
   }
