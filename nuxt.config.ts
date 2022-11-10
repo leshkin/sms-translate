97712f8811580a5496fd5cf22f003d6b0ccc9d63
leshkin <alaposhin@gmail.com>
2022-11-10 16:32:36 +0300

    build: change baseURL for DEV mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
   app: {
-    baseURL: '/sms-translate/',
+    baseURL: process.env.NODE_ENV === 'development' ? '/' : '/sms-translate/',
     head: {
       charset: 'utf-16',
       viewport: 'width=500, initial-scale=1', 
